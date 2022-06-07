507c316a8169abde7c410bdd00ef93a35c399f1c
delefredag <fbmlife0909@gmail.com>
2022-06-07 23:48:02 +0900

    on Tue

---
+++ 3. Fundamentals Part2/46. Iteration: The for Loop.js
@@ -0,0 +1,22 @@
+//violate : don't repeat yourself
+console.log('Lifting weights repetition 1🏋️‍♀️');
+console.log('Lifting weights repetition 2🏋️‍♀️');
+console.log('Lifting weights repetition 3🏋️‍♀️');
+console.log('Lifting weights repetition 4🏋️‍♀️');
+console.log('Lifting weights repetition 5🏋️‍♀️');
+console.log('Lifting weights repetition 6🏋️‍♀️');
+console.log('Lifting weights repetition 7🏋️‍♀️');
+console.log('Lifting weights repetition 8🏋️‍♀️');
+console.log('Lifting weights repetition 9🏋️‍♀️');
+console.log('Lifting weights repetition 10🏋️‍♀️');
+
+//for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+    console.log('lift 10times');
+}
+
+// increase the number in the string that we're printing out
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}🏋️‍♀️`);
+}
+
---
+++ 3. Fundamentals Part2/47. Looping Arrays, Breaking and Continuing.js
@@ -0,0 +1,17 @@
+const jiArray = [
+    'Ji',
+    'Kim',
+    2037-1994,
+    'Student',
+    ['Kim', 'Yang', 'Shin']
+];
+
+//console.log(jiArray[0]);
+//console.log(jiArray[1]);
+// ... 
+//console.log(jiArray[4]);
+
+//array is zero based[0] cuz when it comes to reading elements out of the array.
+for(let i = 0; i<jiArray.length;i++ ){
+    console.log(jiArray[i]);
+}
