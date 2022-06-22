4ab634e1c4b6f025a24dcbe2c37958a38a94de5e
delefredag <fbmlife0909@gmail.com>
2022-06-22 11:24:06 +0900

    DOM

---
+++ 5. Guess my number/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const x = function(){
+    console.log(23);
+}
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess) { //숫자 안 넣고 Check! 버튼 누르면 0,  'number'나온다 0은 falsy value
+        document.querySelector('.message').textContent = '⛔️No number!';
+    }
+});
