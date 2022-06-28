95f8eb69e98a099a427bf4a1aeb52d6b2a6dc6d3
delefredag <fbmlife0909@gmail.com>
2022-06-28 18:35:53 +0900

    if message

--- 5. Guess my number/script.js
+++ 5. Guess my number/script.js
@@ -9,14 +9,24 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const x = function(){
-    console.log(23);
-}
-document.querySelector('.check').addEventListener('click', function(){
+const secretNumber = Math.trunc(Math.random() * 20) + 1; // this logic supposed to be hidden behind question mark
+let score = 20; //let : decrease 
+
+document.querySelector('.number').textContent = secretNumber; // BAAM 
+
+document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
-    if(!guess) { //숫자 안 넣고 Check! 버튼 누르면 0,  'number'나온다 0은 falsy value
+    if (!guess) { //숫자 안 넣고 Check! 버튼 누르면 0,  'number'나온다 0은 falsy value
         document.querySelector('.message').textContent = '⛔️No number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉Correct Number!';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = '📈Too high!';
+        score = score - 1;
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = '📉Too low!';
+        score--;
     }
 });
