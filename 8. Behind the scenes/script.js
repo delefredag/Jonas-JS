0867e18fd5a83958a8002dd7d27cf548af13b631
delefredag <fbmlife0909@gmail.com>
2022-10-20 08:31:01 +0900

    Scoping in Practice

---
+++ 8. Behind the scenes/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function calcAge(birthYear){
+  const age = 2037 - birthYear;
+  
+  function printAge(){
+    const output = `${firstName}, You are ${age}, born in ${birthYear}`
+    console.log(output); 
+
+    if(birthYear >= 1981 && birthYear <= 1996){
+      const str =`Oh, and you're a millennial, ${first}`
+
+      
+  }
+  printAge(); 
+  return age;
+}
+}
+const firstName = 'Global';
+calcAge(1991);
+
+
+
