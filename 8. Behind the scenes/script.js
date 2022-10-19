4cca50be9bb612bc879b64502fe8dd1f9c321295
delefredag <fbmlife0909@gmail.com>
2022-10-20 08:57:40 +0900

    Hoisting and TDZ in Practice

--- 8. Behind the scenes/script.js
+++ 8. Behind the scenes/script.js
@@ -1,23 +1,47 @@
 'use strict';
+ console.log(me);
+// console.log(job);
+// console.log(year);
 
-function calcAge(birthYear){
-  const age = 2037 - birthYear;
-  
-  function printAge(){
-    const output = `${firstName}, You are ${age}, born in ${birthYear}`
-    console.log(output); 
+var me = 'Jonas'
+let job = 'teacher';
+const year= 1991;
 
-    if(birthYear >= 1981 && birthYear <= 1996){
-      const str =`Oh, and you're a millennial, ${first}`
+// Functions
+/**console.log(addDecl(2,3));  //function
+console.log(addExpr(2,3));  //const TDZ error 
+console.log(addArrow(2,3)); */
 
-      
-  }
-  printAge(); 
-  return age;
+
+function addDecl(a,b){
+  return a+b;
 }
+
+const addExpr = function(a,b){
+  return a+b;
 }
-const firstName = 'Global';
-calcAge(1991);
+
+const addArrow = (a,b)=> a+ b;
+// var addExpr. addArrow console 해도 not a function (undefined)
 
 
+// var Example💩
+console.log(undefined); // undefined
+console.log(numProducts); // undefined
+if(!numProducts) deleteShoppingCart();
 
+var numProducts = 10; // 10은 0이 아니고 falsy가 아닌데도 'All products deleted!가 출력됨  - var - undefined 인채로 호이스팅 
+function deleteShoppingCart(){
+  console.log('All products deleted!');
+}
+
+
+var x =1; //window에서는 x:1 값 찾을 수 있음
+let y =2;
+const z = 3;
+//크롬 윈도우 콘솔창
+/**
+console.log(x === window.x);   true
+console.log(y === window.y);   false
+console.log(z === window.z);   false
+*/
