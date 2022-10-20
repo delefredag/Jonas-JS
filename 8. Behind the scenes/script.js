2ac3141ff321f2cca0ca541204c64f0c2a0321c7
delefredag <fbmlife0909@gmail.com>
2022-10-20 10:36:52 +0900

    Regular vs Arrow Functions

--- 8. Behind the scenes/script.js
+++ 8. Behind the scenes/script.js
@@ -1,37 +1,59 @@
 'use strict';
- 
-console.log(this);
-
-const calcAge = function(birthYear){
-  console.log(2037 - birthYear);
-  console.log(this); //undefined in strict mode 
-}
-calcAge(1991);
 
 
-const calcAgeArrow = birthYear=>{
-  console.log(2037 - birthYear);
-  console.log(this); //{} arrow function doesn't get its own this keyword. get lexical parents {window} as global scope instead
-}
-calcAgeArrow(1980);
 
-const jonas ={
+const jonas ={ //not global, it's just block
+  firstName: 'Jonas',
   year: 1991,
   calcAge : function() {
     console.log(this);
     console.log(2037-this.year);
-  }
+
+    /** Solution 1
+    const self = this; // self or that
+    const isMillenial = function(){
+      console.log(self.year >= 1981 && self.year <= 1996); //
+      //console.log(this.year >= 1981 && this.year <= 1996); //year undefined
+    };
+    */
+
+    // Solution 2 arrow function
+    const isMillenial = ()=>{
+      console.log(this); //A function uses this keyword in the parent scope (inherited)
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+
+
+    isMillenial();
+  },
+  greet: ()=> { //undefined
+    console.log(`Hey ${this.firstName}`); //arrow function doesn't get its own this keyword
+ },
+
+ /** Use regular function instead
+ greet : function(){
+  console.log(this);
+  console.log(`Hey ${this.firstName}`);
+ }
+ */
 }
-jonas.calcAge();
 
-const matilda = {
-  year :2017,
+jonas.greet(); //Hey undefined
+console.log(this.firstName); //undefined :Window Object /there's no first name
+
+jonas.calcAge(); //year undefined, so add const self
+
+
+// Arguments keyword
+const addExpr = function (a,b){
+  console.log(arguments);
+  return a + b;
 };
-matilda.calcAge = jonas.calcAge;
-console.log(matilda);
-console.log(jonas);
+addExpr(2,5);
+addExpr(2,5, 8, 12);
 
-matilda.calcAge();
-
-const f = jonas.calcAge;
-f(); //app crashed
+var addArrow = (a,b) => {
+  console.log(arguments);
+  return a+b;
+};
+addArrow(2, 5, 8);
